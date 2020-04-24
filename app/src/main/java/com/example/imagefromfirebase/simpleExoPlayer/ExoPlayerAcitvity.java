38e966e0b55e739f032b2228588399e50f51d17e
Shekhar <shekhar59324@gmail.com>
2020-04-24 14:03:54 +0530

    Minor code changes in VideoPlayer Activity

--- app/src/main/java/com/example/imagefromfirebase/simpleExoPlayer/ExoPlayerAcitvity.java
+++ app/src/main/java/com/example/imagefromfirebase/simpleExoPlayer/ExoPlayerAcitvity.java
@@ -2,9 +2,11 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.example.imagefromfirebase.R;
 import com.google.android.exoplayer2.ExoPlayerFactory;
@@ -21,32 +23,62 @@ import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ktx.Firebase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
  public class ExoPlayerAcitvity extends AppCompatActivity {
 
     SimpleExoPlayer exoPlayer;
     SimpleExoPlayerView exoPlayerView;
-     String videoURL = "http://blueappsoftware.in/layout_design_android_blog.mp4";
+
+     FirebaseStorage storage = FirebaseStorage.getInstance();
+     StorageReference storageRef = storage.getReference();
+     StorageReference pathReference = storageRef.child("1AdvancedKotlinIntro.mp4");
+
+
+     String videoURL;
+     TextView uri;
 
      @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_exo_player);
-
         exoPlayerView = (SimpleExoPlayerView) findViewById(R.id.exo_player_view);
-        try {
-            setupVideoPlayer();
-
-        }catch (Exception e){
-            Log.e("ExoPlayerActivity", "exoplayer error"+ e.toString());
-        }
+        getUrl();
     }
+
+     private void getUrl() {
+         uri =findViewById(R.id.uri);
+         pathReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>()
+         {
+             @Override
+             public void onSuccess(Uri downloadUrl)
+             {
+                 videoURL =downloadUrl.toString();
+                 Log.d("====>",videoURL);
+                 uri.setText(videoURL);
+                 try {
+                     setupVideoPlayer();
+
+                 }catch (Exception e){
+                     Log.e("ExoPlayerActivity", "exoplayer error"+ e.toString());
+                 }
+
+             }
+         });
+
+     }
 
      private void setupVideoPlayer() {
 
          BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
          TrackSelector trackSelector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(bandwidthMeter));
          exoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+
+
+
 
          Uri videoURI = Uri.parse(videoURL);
          DefaultHttpDataSourceFactory dataSourceFactory = new DefaultHttpDataSourceFactory("exoplayer_view");
