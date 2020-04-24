64d7ac0b892f614fcb91dd6d2fd6a36299ac24a8
Shekhar <shekhar59324@gmail.com>
2020-04-24 22:51:35 +0530

    RecyclerView Activity & adapter Setup

--- app/src/main/java/com/example/imagefromfirebase/videoPlayerRecyclerView/VideoPlayerRecyclerViewActivity.java
+++ app/src/main/java/com/example/imagefromfirebase/videoPlayerRecyclerView/VideoPlayerRecyclerViewActivity.java
@@ -1,16 +1,50 @@
 package com.example.imagefromfirebase.videoPlayerRecyclerView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.imagefromfirebase.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 public class VideoPlayerRecyclerViewActivity extends AppCompatActivity {
 
+    FirebaseFirestore db;
+    RecyclerView mRecyclerView;
+    ArrayList<VideoPlayer_card> videoPlayerCardArrayList;
+    VideoPlayerRecyclerViewAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player_recycler_view);
+
+        setUpFirebase();
+
+
+        setUpRecyclerView();
     }
+
+    private void setUpFirebase() {
+        db = FirebaseFirestore.getInstance();
+    }
+
+
+    private void setUpRecyclerView() {
+        mRecyclerView =findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+
 }
